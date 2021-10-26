add2132c1009fc54cea1c12bef02ac75db6c2a30
kimjisu33 <indexkim33@gmail.com>
2021-10-26 11:14:33 +0900

    장애물 생성

--- GameStart/game.js
+++ GameStart/game.js
@@ -4,7 +4,7 @@ let gameCanvas;
 let context;
 
 let backgroundImg=new Image();
-backgroundImg.src="../img/game_gaugeBackground.png";
+backgroundImg.src="../img/game_gaugeBackground.png";//배경 나중에 바꾸기
 let charImg=new Image();
 let ObstacleImg=new Image();
 charImg.src="../img/char_ready.png"
@@ -16,23 +16,34 @@ let Obs_y=0; //장애물 y 좌표
 let dx=0;
 let dy=0;
 let Spacekey=1; //스페이스 종류 판단
-
 let keycode;
 setInterval(dy=2,1);
+
+let set=new Set();
+
 function gameStart(){
   gameCanvas = document.getElementById("gameCanvas");
   context = gameCanvas.getContext("2d");
   gameCanvas.width=800;
   gameCanvas.height=600;
 
-  runGame();
-  setInterval(runGame,1);
+  
+  createObstacle();
+  let run=setTimeout(function runGame(){
+    move();
+    draw();
+    run=setTimeout(runGame,1);
+  },1);
+
+
+  //setInterval(runGame,1);
 }
 
+/*
 function runGame(){
   move();
   draw();
-}
+}*/
 
 function move() {
   if(char_x+dx>0 && char_x+dx<700 ){
@@ -44,10 +55,16 @@ function move() {
   }
 }
 function draw() {
+ 
   context.drawImage(backgroundImg, 0,0,800,600); // 배경 그리기
+  for(let block of set){ //장애물 그리기
+    context.drawImage(ObstacleImg,block.x, block.y, 100,100);
+  }
   context.drawImage(charImg, char_x,char_y, 100,100); // 캐릭터 그리기
-  Obstacle();
-  context.fillText(keycode,10, 40);
+  //context.drawImage(ObstacleImg,block.x, block.y,100,100);
+  //Obstacle(); //장애물 그리기
+  //context.fillText(keycode,10, 40); //키보드 이벤트 확인용
+  
 }
 
 function keydown() {
@@ -89,19 +106,24 @@ function Space(){
 }
 
 //장애물 생성
-function Obstacle(){
+function createObstacle(){
   let x=[0,100,200,300,400,500,600,700];
   let y=[0,100,200];
-  Obs_x=Math.floor(Math.random() * 8);
-  Obs_y= Math.floor(Math.random() * 3);
 
+  for(let i=0 ; i<10 ; i++){
+    Obs_x=Math.floor(Math.random() * 8);
+    Obs_y= Math.floor(Math.random() * 3);
+    set.add({
+      x: x[Obs_x],
+      y: y[Obs_y],
+    });
+  } 
+  
 
   //장애물 10개 생성하는데 왜그러지??
+  /*
   for(let i=0; i<10; i++){
-    context.drawImage(ObstacleImg,x[Obs_x],y[Obs_y], 100,100);
-  }
+    context.drawImage(ObstacleImg,block.x, block.y, 100,100);
+  }*/
 
-
-
-  
 }
